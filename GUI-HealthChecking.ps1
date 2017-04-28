eace9293db66f5cbd1d93902551cef880f271b27
Alexey A. Leonov <alexey@storj.io>
2017-04-29 00:13:34 +0300

    * first version

---
+++ GUI-HealthChecking.ps1
@@ -0,0 +1,18 @@
+﻿Get-Item '~\AppData\Roaming\Storj Share\*.log' |%{
+    sls '\[(.*)\].* publish' $_ | select -last 1 | % {
+        write-host $_.Filename; 
+        Write-Host "last publish ("(sls 'publish' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }; 
+    sls '\[(.*)\].* offer' $_ | select -last 1 | % {
+        Write-Host "last offer ("(sls 'offer' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }; 
+    sls '\[(.*)\].* consign' $_ | select -last 1 | % {
+        write-host "last consign ("(sls 'consign' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }; 
+    sls '\[(.*)\].* download' $_ | select -last 1 | % {
+        write-host "last download ("(sls 'download' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }; 
+    sls '\[(.*)\].* upload' $_ | select -last 1 | % {
+        write-host "last upload ("(sls 'upload' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }
+}
