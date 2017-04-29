383f13d149b28c5e7d38f34c9accb4b39b2bc04c
Alexey A. Leonov <alexey@storj.io>
2017-04-29 14:18:23 +0300

    + pulse checking for daemon

---
+++ daemon-HealthChecking.ps1
@@ -0,0 +1,18 @@
+﻿Get-Item ~\.config\storjshare\logs\*.log |%{
+    sls 'publish .*timestamp":"(.*)"' $_ | select -last 1 | % {
+        write-host $_.Filename; 
+        Write-Host "last publish ("(sls 'publish' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }; 
+    sls 'offer .*timestamp":"(.*)"' $_ | select -last 1 | % {
+        Write-Host "last offer ("(sls 'offer' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }; 
+    sls 'consign.*timestamp":"(.*)"' $_ | select -last 1 | % {
+        write-host "last consign ("(sls 'consign' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }; 
+    sls 'download.*timestamp":"(.*)"' $_ | select -last 1 | % {
+        write-host "last download ("(sls 'download' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }; 
+    sls 'upload.*timestamp":"(.*)"' $_ | select -last 1 | % {
+        write-host "last upload ("(sls 'upload' $_.Path).Matches.Count"):" $_.matches.groups[1].value
+    }
+}
