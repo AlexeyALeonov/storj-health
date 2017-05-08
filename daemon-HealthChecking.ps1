df9a10bc806c9973c134c2ce20eb636bd61fe144
Alexey A. Leonov <alexey@storj.io>
2017-05-08 14:39:38 +0300

    + checking private + checking opcodes

--- daemon-HealthChecking.ps1
+++ daemon-HealthChecking.ps1
@@ -8,26 +8,35 @@ Get-Item (Join-Path $Path *.log) |%{
     Write-Host $file.Name;
     Write-Host
 
+    $isPrivate = $null
     sls 'you are not publicly reachable' $file | select -Last 1 | %{Write-Host ('```'+$_.Line+'```')}
     sls 'no public' $file | select -Last 1 | %{Write-Warning ('```'+$_.Line+'``` <-- *bad*')}
-    sls 'private ' $file | select -Last 1 | %{Write-Warning ('```'+$_.Line+'``` <-- *bad*')}
+    $isPrivate = sls 'private ' $file | select -Last 1 | %{('```'+$_.Line+'``` <-- *bad*')}
+
+    $opcode1 = $null
+    $opcode2 = $null
+    $opcode3 = $null
+    $opcode1 = sls 'subscribing to topic \"0f01020202\"' $file | select -Last 1
+    $opcode2 = sls 'subscribing to topic \"0f02020202\"' $file | select -Last 1
+    $opcode3 = sls 'subscribing to topic \"0f03020202\"' $file | select -Last 1
 
     $upnp = $null
     $address = $null
     $port = $null
     $delta = $null
 
-    $upnp = sls 'message":"(.* upnp.*?)"' $file | select -last 1 | % {$_.Matches.Groups[1].Value}
+    $upnp = sls 'message":"(.* upnp.*?)"' $file | select -last 1 | % {$_.Line}
     if (-not $upnp) {
         sls 'message":"(.* public.*?)"' $file | select -last 1 | % {$_.Matches.Groups[1].Value}
     } else {
         if (($upnp | sls 'successful').Matches.Success) {
-            Write-Host ('`'+$upnp+'` <-- *not optimal*')
+            Write-Host ('```'+$upnp+'``` <-- *not optimal*')
         } else {
-            Write-Warning ('`'+$upnp+'` <-- *bad*')
+            Write-Warning ('```'+$upnp+'``` <-- *bad*')
         }
         ($address, $port) = sls "upnp: (.*):(.*)" $file | 
             select -last 1 | %{$_.matches.Groups[1].value, $_.Matches.Groups[2].value}
+        Write-Host
     }
 
     sls "sharddata.kfs" $file | select -last 1 | % {Write-Warning $_.Line}
@@ -105,6 +114,18 @@ Get-Item (Join-Path $Path *.log) |%{
     }
 
     Write-Host "--------------"
+    if (-not $opcode1) {Write-Host '`Opcode "0f01020202" not found!`  <-- *bad*'}
+    if (-not $opcode2) {Write-Host '`Opcode "0f02020202" not found!`  <-- *bad*'}
+    if (-not $opcode3) {Write-Host '`Opcode "0f03020202" not found!`  <-- *bad*'}
+    if (-not $opcode1 -or -not $opcode2 -or -not $opcode3) {
+        Write-Host 'You should specify only that opcodes:
+        "0f01020202"
+        "0f02020202"
+        "0f03020202"
+
+        Please, check it in your config!'
+        Write-Host
+    }
     if ($delta -and ($delta -ge 500.0 -or $delta -le -500.0)) {
         Write-Warning ('clock delta: `' + $delta + '` <-- *bad*')
         Write-Host "        Your clock is out of sync
@@ -113,16 +134,23 @@ Get-Item (Join-Path $Path *.log) |%{
         then download this software http://www.timesynctool.com and use ntp server that you found out in previous step
         "
     }
+    if ($isPrivate) {
+        Write-Host $isPrivate
+        Write-Host '        Please, check your `"rpcAddress"` option in configuration of node.'
+        if ($address) {Write-Host ('        It is should be like this: `"rpcAddress": "' + $address + '",`')}
+        Write-Host
+    }
     if ($upnp) {
         if (($upnp | sls 'successful').Matches.Success) {
-            Write-Host ('`'+$upnp+'` <-- *not optimal*')
+            Write-Host ('```'+$upnp+'``` <-- *not optimal*')
         } else {
-            Write-Warning ('`'+$upnp+'` <-- *bad*')
+            Write-Warning ('```'+$upnp+'``` <-- *bad*')
         }
     }
     if (-not $isPortOpen -and $port -and $address -and -not $isTunneling) {
         Write-Warning ('`port ' + $port + ' is CLOSED on ' + $address +'` <-- *bad*')
         Write-Host 'Please, check it:' http://www.yougetsignal.com/tools/open-ports/
+        Write-Host
     }
     if ($isTunneling) {
         Write-Host '`You are using tunneling` <-- *not optimal*'
